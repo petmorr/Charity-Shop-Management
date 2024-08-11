b58fac6b99eab2288bd4aecc855a323179c04b42
petmorr <peterm385@hotmail.co.uk>
2024-08-11 23:50:37 +0100

    Update express-validator to version 7.2.0

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,4 +1,4 @@
-const { check, validationResult } = require("express-validator");
+const { validationResult } = require("express-validator");
 
 // Controller function to handle GET request for managing items
 exports.getManageItems = (req, res, itemsDb, logger) => {
